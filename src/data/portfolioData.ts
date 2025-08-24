00c2ac7009e8815d602346774a3d687c36439846
Luke <155145089+lukegabriel520@users.noreply.github.com>
2025-08-24 14:30:24 +0800

    Update portfolioData.ts

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -264,7 +264,7 @@ export const testimonials: Testimonial[] = [
 export const personalInfo = {
   name: "Luke Lumakin",
   role: "Associate Data Analyst",
-  slogan: "Intent in every line. Substance in every thought. Innovate, lest, reproducible brilliance.",
+  slogan: "Intent. Substance. Innovate, lest, reproducible brilliance.",
   languages: ["Filipino", "English"],
   status: "Blockchain Intern",
   school: "Pamantasan ng Lungsod ng Maynila (PLM)",
